a972c593c3ab827f040c2d190544cee0a337f1d2
Jean-Yves NOLEN <jynolen+opensource@gmail.com>
2020-05-27 14:26:37 +0200

    Missing typo in injector

--- injector.js
+++ injector.js
@@ -1,12 +1,12 @@
-function injectScript(file, node) {
-    var th = document.getElementsByTagName(node)[0];
-    var s = document.createElement('script');
+let injectScript = function(file, node) {
+    let th = document.getElementsByTagName(node)[0];
+    let s = document.createElement('script');
     s.setAttribute('type', 'text/javascript');
     s.setAttribute('src', file);
     th.appendChild(s);
 }
 
-function checkForSplunk()
+let checkForSplunk = function()
 {
     const metas = document.getElementsByTagName('meta');
     for (let i = 0; i < metas.length; i++) {
@@ -14,7 +14,7 @@ function checkForSplunk()
             return metas[i].getAttribute('content') === "Splunk Inc.";
         }
     }
-    return False
+    return false;
 };
 
 if(checkForSplunk())
@@ -22,5 +22,3 @@ if(checkForSplunk())
     injectScript( chrome.extension.getURL('splunk_xml.js'), 'body' );
     injectScript( "//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/highlight.min.js", 'body');
 }
-
-console.log("Ok module load")
